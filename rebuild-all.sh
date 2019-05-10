28e8243b18ff2c61ec85b5d7f6cafc748da73fb8
Juri Boxberger <juri.boxberger@gmx.net>
2019-05-11 01:42:22 +0200

    update to 11.10.4

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -39,7 +39,8 @@ declare -A versions;      declare -a orders;
 #versions["11.8.2"]="808"; orders+=( "11.8.2" )
 #versions["11.8.3"]="808"; orders+=( "11.8.3" )
 #versions["11.9.5"]="838"; orders+=( "11.9.5" )
-versions["11.9.8"]="842"; orders+=( "11.9.8" )
+#versions["11.9.8"]="842"; orders+=( "11.9.8" )
+versions["11.10.4"]="855"; orders+=( "11.10.4" )
 
 for i in "${!orders[@]}"
 do
